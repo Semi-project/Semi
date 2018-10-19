a1a8c8bc416384a7f1725d81a103d00fbc93cd1f
jinw <nada2781@naver.com>
2018-10-19 16:01:16 +0900

    controller/board/qnaboard
    컨트롤단 추가

--- src/controller/board/qnaboard/QnABoardUpdateController.java
+++ src/controller/board/qnaboard/QnABoardUpdateController.java
@@ -12,33 +12,48 @@ import dto.board.QnA;
 import service.board.QnAService;
 import service.board.QnAServiceImpl;
 
-@WebServlet("/qnaboard/write")
-public class QnABoardWriteController extends HttpServlet {
+
+@WebServlet("/qnaboard/update")
+public class QnABoardUpdateController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	private QnAService qnaService = new QnAServiceImpl();
 	
-	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 	
+		QnA qna = qnaService.getParam(req, resp);
 		
-		req.getSession();
+//		if( !qnaService.che ) {
+//			resp.sendRedirect("/qnaboard/list");
+//			return;
 		
-		req.getRequestDispatcher("/view/board/qna/write.jsp").forward(req, resp);
-	}
+		// 게시글 조회수행
+		QnA qnaView = qnaService.viewQnA(qna);
 	
+		// MODEL 전달
+		req.setAttribute("qnaView", qnaView);
+		
+		//글 작성자 닉네임 전달
+		
+		// 첨부파일 전달
+//		QnA_Filetb qna_Filetb = qnaService.
+		
+		//view 지정
+		req.getRequestDispatcher("/view/board/qna/update.jsp").forward(req, resp);
+	
+	}
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 	
-	
-		
 		req.setCharacterEncoding("UTF-8");
 		
-		QnA qna = qnaService.getParam(req, resp);
+		qnaService.updateQnA(req);
 		
-		//System.out.println(qna.getBoardno());
-		qnaService.writeQnA(req);
 		resp.sendRedirect("/qnaboard/list");
+		
 	}
+	
+	
+	
 }
