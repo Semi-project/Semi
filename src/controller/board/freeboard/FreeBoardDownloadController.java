a403a5431fa71344e299cf441f4bd3ce1ce70665
whitekk3 <whitekk26@gmail.com>
2018-10-23 15:31:39 +0900

    2018.10.23/ freeboard 작업 합니다.

--- src/controller/board/freeboard/FreeBoardDownloadController.java
+++ src/controller/board/freeboard/FreeBoardDownloadController.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-
+//freeboard수정합니다
 import dao.board.Free_BoardDao;
 import dao.board.Free_BoardDaoImpl;
 import dao.file.Free_FileDao;
